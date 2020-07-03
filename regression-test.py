ec9d3ed3acbf2322f1d830a086e23b0e9a886fab
bigginlab <philip.biggin@bioch.ox.ac.uk>
2020-07-03 13:26:08 +0100

    flake8 consistent

--- regression-test.py
+++ regression-test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# encoding: utf-8
 '''
 Simple regression-type test to check output from WaterDock-2.0 is as expected after installation
 
@@ -7,27 +9,20 @@ It will generate predictedwaters.pdb
 
 Test compares this to expected-predictedwaters.pdb
 
-
 '''
 
-#!/usr/bin/python
-
-import sys
 import os
 
 os.system("python ./waterdock2.py example-protein.pdbqt example-ligand.pdb")
 
-f1=open("expected-predictedwaters.pdb","r")
-f2=open("predictedwaters.pdb","r")
+f1 = open("expected-predictedwaters.pdb", "r")
+f2 = open("predictedwaters.pdb", "r")
 for line1 in f1:
-	for line2 in f2:
-		if line1==line2:
-			print("Output is as expected - test passed\n")
-		else:
-			print(line1 + line2)
-		break
+    for line2 in f2:
+        if line1 == line2:
+            print("Output is as expected - test passed\n")
+        else:
+            print(line1 + line2)
+        break
 f1.close()
 f2.close()
-
-
-
--- waterdock2.py
+++ waterdock2.py
@@ -15,47 +15,45 @@ DEPENDENCIES:
 '''
 import sys
 import os
+import addwater
+import dockcheck
+
 
 def waterfile():
-	f1 = open('water.pdbqt', 'w')
-
-	f1.write('REMARK  The pdbqt file for using water as a ligand\n')
-	f1.write('ROOT\n')
-	f1.write('ATOM      1  OW  HOH   231       0.950  11.375  16.494  1.00  0.00    -0.411 OA\n')
-	f1.write('ATOM      2  HW1 HOH   231       1.766  11.375  17.071  1.00  0.00     0.205 HD\n')
-	f1.write('ATOM      3  HW2 HOH   231       0.134  11.375  17.071  1.00  0.00     0.205 HD\n')
-	f1.write('ENDROOT\n')
-	f1.write('TORSDOF 0')
-
-	f1.close()
+    f1 = open('water.pdbqt', 'w')
+    f1.write('REMARK  The pdbqt file for using water as a ligand\n')
+    f1.write('ROOT\n')
+    f1.write('ATOM      1  OW  HOH   231       0.950  11.375  16.494  1.00  0.00    -0.411 OA\n')
+    f1.write('ATOM      2  HW1 HOH   231       1.766  11.375  17.071  1.00  0.00     0.205 HD\n')
+    f1.write('ATOM      3  HW2 HOH   231       0.134  11.375  17.071  1.00  0.00     0.205 HD\n')
+    f1.write('ENDROOT\n')
+    f1.write('TORSDOF 0')
+    f1.close()
 
 
 proteinfile, ligandfile = sys.argv[1], sys.argv[2]
 
 if int(os.path.isfile(proteinfile)) == 0:
-	sys.exit('Protein File does not exist')
+    sys.exit('Protein File does not exist')
 
 if int(os.path.isfile(ligandfile)) == 0:
-	sys.exit('Ligand File does not exist')
+    sys.exit('Ligand File does not exist')
 
 ext1 = proteinfile[-5:]
 ext2 = ligandfile[-4:]
 
 if ext1 != 'pdbqt':
-	sys.exit('Protein File must be in pdbqt format')
+    sys.exit('Protein File must be in pdbqt format')
 
 if ext2 != '.pdb' and ext2 != 'mol2':
-	sys.exit('Ligand File must be in pdb/mol2 format')
+    sys.exit('Ligand File must be in pdb/mol2 format')
 
 if os.path.isfile('predictedwaters.pdb'):
-	os.rename('predictedwaters.pdb','predictedwaters1.pdb')
+    os.rename('predictedwaters.pdb', 'predictedwaters1.pdb')
 
 waterfile()
 
-import addwater
 addwater.main(ligandfile)
-
-import dockcheck
 dockcheck.main(proteinfile, ligandfile)
 
 os.remove('waterdetails.txt')
